c1e72ca9226399746c735ea39148758d804d63ee
jquinnie <jenn8866@gmail.com>
2019-01-14 16:19:29 -0700

    Refactor solution

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,11 +1,5 @@
 export const isLeap = (year) => {
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      if (year % 400 === 0) {
-        return true;
-      }
-      return false;
-    }
+  if ((year % 400 === 0) || (year % 4 === 0 && year % 100)) {
     return true;
   }
   return false;
