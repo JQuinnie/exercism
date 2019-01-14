cf7a9ffb85623f35895188ab0a33bd4fa76b1084
jquinnie <jenn8866@gmail.com>
2019-01-14 16:02:49 -0700

    Refactor from comments

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,14 +2,12 @@ class Matrix
   attr_accessor :rows, :columns
 
   def initialize(str)
-    @rows = row(str)
+    @rows = parse_row(str)
     @columns = column(rows)
   end
 
-  def row(input_str)
-    array = input_str.split("\n")
-    array2 = array.map { |row| row.split(' ') }
-    array2.map { |row| row.map(&:to_i) }
+  def parse_row(input_str)
+    input_str.split("\n").map { |row| row.split(' ').map(&:to_i) }
   end
 
   def column(rows)
