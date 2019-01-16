fd331df0240509c10d05fae205cf92e39f4025c8
jquinnie <jenn8866@gmail.com>
2019-01-15 20:13:29 -0700

    Make refactoring change

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,6 +1,1 @@
-export const isLeap = (year) => {
-  if ((year % 400 === 0) || (year % 4 === 0 && year % 100)) {
-    return true;
-  }
-  return false;
-};
+export const isLeap = year => ((year % 400 === 0) || (year % 4 === 0 && year % 100));
