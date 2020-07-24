d349409fc3c83fd06796cc53b2d5c496decd6e47
jquinnie <jenn8866@gmail.com>
2020-07-24 12:21:52 -0600

    Update changes per mentor advice

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,16 +1,13 @@
 class Matrix
   attr_accessor :rows, :columns
 
-  def initialize(str)
-    @rows = parse_row(str)
-    @columns = column(rows)
+  def initialize(input_string)
+    @rows = parse_rows(input_string)
+    @columns = rows.transpose
   end
 
-  def parse_row(input_str)
-    input_str.split("\n").map { |row| row.split(' ').map(&:to_i) }
+  def parse_rows(string)
+    string.each_line.map { |line| line.split.map(&:to_i) }
   end
 
-  def column(rows)
-    rows.transpose
-  end
 end
